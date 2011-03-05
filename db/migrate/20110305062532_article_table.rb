344b55234bfa5bb483739353d50724a777202cae
Said <said.kaldubaev@gmail.com>
2011-03-05 01:48:34 -0500

    Added article migration

---
+++ db/migrate/20110305062532_article_table.rb
@@ -0,0 +1,12 @@
+class ArticleTable < ActiveRecord::Migration
+  def self.up
+    create_table :articles do |t|
+        t.string :title
+        t.text :content
+        t.timestamps
+      end
+    end
+  def self.down
+    drop_table :articles
+  end
+end
